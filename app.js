3ce4413c1685b5e5ba591f44574de489c57403e2
ismaildev49 <ismaelbentatou@gmail.com>
2023-12-04 17:51:56 +0100

    ...

--- app.js
+++ app.js
@@ -5,29 +5,34 @@ const cookieParser = require('cookie-parser')
 const routes = require('./routes/routes')
 const app = express()
 const PORT = process.env.PORT || 3001
-const dbURI = process.env.MONGODB_URI || process.env.DBURI;
+const dbURI = process.env.MONGODB_URI || process.env.DB_URI;
 
 // Middleware
 // app.use(express.static('public'));
 app.use(express.json())
 app.use(cookieParser())
 
-app.set("views", __dirname + "/views");
-// app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public"));
 //view engine
-app.set('view engine', 'ejs')
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+
 
 
 // Database Connection
 
-mongoose.connect(process.env.dbURI)
+mongoose.connect(dbURI)
   .then((result) => app.listen(PORT, (err, data) => {
     console.log('listening to port ' + PORT);
   }))
   .catch((err) => console.log(err));
 
 
+
 //routes
 
 app.use(routes)
+
+
+module.exports = app;
